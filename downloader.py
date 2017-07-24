388dfb914a2960b62b6b4323f80b924dc5ab19bf
pixelpython <alexanderbarran@gmail.com>
2017-07-24 09:27:24 -0700

    fixed Windows compatability

--- downloader.py
+++ downloader.py
@@ -1,25 +1,17 @@
-import requests;
-import re;
-import sys;
-import os;
+import requests, re, sys
 
-PY3 = sys.version_info[0] == 3
-if PY3:
+if sys.version_info[0] == 3:
     from urllib.request import urlretrieve
 else:
     from urllib import urlretrieve
-links = sys.argv[1:];
+links = sys.argv[1:]
 downloadlink = "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=47.0&x=id%3D{}%26installsource%3Dondemand%26uc"
-extensionids = [];
-regexx = re.compile("http[s]?.+\/(?P<lastpart>.+)");
+extensionids = []
+regexx = re.compile("http[s]?.+\/(?P<lastpart>.+)")
+requests.packages.urllib3.disable_warnings()
 for link in links:
-    extensionids.append(regexx.search(link).group("lastpart"));
-
+    extensionids.append(regexx.search(link).group("lastpart"))
 for extensionid in extensionids:
-    link = downloadlink.format(extensionid);
-    extensiondownload = requests.get(link,headers={"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36"}).url;
-    urlretrieve(extensiondownload,"/tmp/{}.crx".format(extensionid));
-    if sys.platform == "darwin":
-      os.system("open /Applications/Chromium.app /tmp/%s.crx" % extensionid);
-    else:
-      os.system("inox /tmp/%s.crx" % extensionid);
+    link = downloadlink.format(extensionid)
+    extensiondownload = requests.get(link,verify=False,headers={"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36"}).url
+    urlretrieve(extensiondownload,"{}.crx".format(extensionid))
